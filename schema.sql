dfe48c6c37a50b598ed940d6c4351e945450bf0c
Rob Bronson <robert.j.bronson+cf@gmail.com>
2019-05-03 10:00:08 -0700

    fixed schema not dropping tables

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS weather, yelp, movies, trails, locations;
+DROP TABLE IF EXISTS weather, events, yelp, movies, trails, locations;
 
 
 CREATE TABLE IF NOT EXISTS locations (
-  id  SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   search_query VARCHAR(255),
   formatted_query VARCHAR(255),
   latitude NUMERIC(10,7),
@@ -10,16 +10,16 @@ CREATE TABLE IF NOT EXISTS locations (
 );
 
 CREATE TABLE IF NOT EXISTS weather(
-  id  SERIAL PRIMARY KEY,
-  location_id INTEGER REFERENCES location(id),
+  id SERIAL PRIMARY KEY,
+  location_id INTEGER REFERENCES locations(id),
   time_stamp BIGINT,
   forecast VARCHAR(255),
   weather_time VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS events(
-  id  SERIAL PRIMARY KEY,
-  location_id INTEGER REFERENCES location(id),
+  id SERIAL PRIMARY KEY,
+  location_id INTEGER REFERENCES locations(id),
   time_stamp BIGINT,
   link VARCHAR(255),
   event_name VARCHAR(255),
@@ -28,8 +28,8 @@ CREATE TABLE IF NOT EXISTS events(
 );
 
 CREATE TABLE IF NOT EXISTS yelp(
-  id  SERIAL PRIMARY KEY,
-  location_id INTEGER REFERENCES location(id),
+  id SERIAL PRIMARY KEY,
+  location_id INTEGER REFERENCES locations(id),
   time_stamp BIGINT,
   yelp_name VARCHAR(255),
   image_url VARCHAR(255),
@@ -39,11 +39,11 @@ CREATE TABLE IF NOT EXISTS yelp(
 );
 
 CREATE TABLE IF NOT EXISTS movies(
-  id  SERIAL PRIMARY KEY,
-  location_id INTEGER REFERENCES location(id),
+  id SERIAL PRIMARY KEY,
+  location_id INTEGER REFERENCES locations(id),
   time_stamp BIGINT,
   movie_title VARCHAR(255),
-  overview VARCHAR(1000)
+  overview VARCHAR(1000),
   avg_votes NUMERIC,
   total_votes NUMERIC,
   image_url VARCHAR(255),
@@ -52,8 +52,8 @@ CREATE TABLE IF NOT EXISTS movies(
 );
 
 CREATE TABLE IF NOT EXISTS trails(
-  id  SERIAL PRIMARY KEY,
-  location_id INTEGER REFERENCES location(id),
+  id SERIAL PRIMARY KEY,
+  location_id INTEGER REFERENCES locations(id),
   time_stamp BIGINT,
   trail_name VARCHAR(255),
   formatted_location VARCHAR(255),
