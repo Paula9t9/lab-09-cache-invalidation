b6f6ebe71fc714b3d151c2e3d31c25e011cd57da
Paula Thomas <paula9t9@gmail.com>
2019-05-03 11:04:13 -0700

    Most bugs cleaned up

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ function Location(query, res) {
   this.latitude = res.results[0].geometry.location.lat;
   this.longitude = res.results[0].geometry.location.lng;
 }
+
 function Weather(weatherRes, time) {
   this.forecast = weatherRes;
   this.time = new Date(time * 1000).toDateString();
@@ -48,24 +49,26 @@ app.get('/location', (request, response) => {
         //use data from db and send result
         response.status(200).send(data.rows[0]);
       } else {
-        let dataFile = `https://maps.googleapis.com/maps/api/geocode/json?address=${queryData}&key=${
-          process.env.GOOGLE_MAPS_API_KEY
-        }`;
+        let dataFile = `https://maps.googleapis.com/maps/api/geocode/json?address=${queryData}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
         superagent.get(dataFile).end((err, googleMapsApiResponse) => {
-          // console.log(queryData);
+          console.log(`googleMapsApiResponse: ${googleMapsApiResponse.body.status}`);
           const locationObject = new Location(
             queryData,
             googleMapsApiResponse.body
           );
           let insertStatement =
-            'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ( $1, $2, $3, $4);';
+            'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ( $1, $2, $3, $4) ON CONFLICT DO NOTHING RETURNING id;';
           let insertValue = [
             locationObject.search_query,
             locationObject.formatted_query,
             locationObject.latitude,
             locationObject.longitude
           ];
-          client.query(insertStatement, insertValue);
+          client.query(insertStatement, insertValue)
+            .then(result => {
+              this.id = result.rows[0].id;
+              return this;
+            });
           response.status(200).send(locationObject);
         });
       }
@@ -77,30 +80,21 @@ app.get('/location', (request, response) => {
 });
 
 app.get('/weather', (request, response) => {
-  response
-    .status(200)
-    .send(
-      lookupFunction(
-        request.query.data,
-        'weather',
-        weatherDbFetcher,
-        weatherApiFetcher
-      )
-    );
-  // try {
-  //   const queryData = request.query.data;
-  //   let dataFile = `https://api.darksky.net/forecast/${process.env.DARKSKY_KEY}/${queryData.latitude},${queryData.longitude}`;
-  //   superagent.get(dataFile)
-  //     .end((err, weatherApiResponse) => {
-  //       let weatherForecastMap = weatherApiResponse.body.daily.data.map(element=>{
-  //         return new Weather(element.summary,element.time);
-  //       });
-  //       response.status(200).send(lookupFunction());
-  //     });
-  // } catch(error){
-  //   console.log(error);
-  //   response.status(500).send('There is an error on our end sorry');
-  // }
+  try {
+    response
+      .status(200)
+      .send(
+        lookupFunction(
+          request.query.data,
+          'weather',
+          weatherDbFetcher,
+          weatherApiFetcher
+        )
+      );
+  }catch (error) {
+    console.error(error);
+    response.status(500).send('Sorry, something went wrong');
+  }
 });
 
 app.get('/events', (request, response) => {
@@ -109,16 +103,13 @@ app.get('/events', (request, response) => {
     let dataFile = `https://www.eventbriteapi.com/v3/events/search?location.longitude=${
       queryData.longitude
     }&location.latitude=${queryData.latitude}`;
-    // console.log(dataFile);
     superagent
       .get(dataFile)
       .set({ Authorization: `Bearer ${process.env.EVENTBRITE_KEY}` })
       .end((err, eventBriteApiResponse) => {
-        // console.log(eventBriteApiResponse.body.events);
         let eventMap = eventBriteApiResponse.body.events.map(
           element => new Event(element)
         );
-        // console.log(eventMap);
         response.status(200).send(eventMap);
       });
   } catch (error) {
@@ -127,61 +118,59 @@ app.get('/events', (request, response) => {
   }
 });
 
-// response.status(200).send(lookupFunction(search_query, weatherDbFetcher, weatherApiFetcher));
-
-function lookupFunction(searchQuery, table, dbFetcher, apiFetcher) {
-  console.log(`Search query: ${searchQuery.search_query}`);
-  console.log(`table: ${table}`);
+function lookupFunction(data, table, dbFetcher, apiFetcher) {
   //if locations contains searchQuery, execute other functions to fetch their data
-  let sqlStatement = `SELECT * FROM ${table} WHERE search_query = $1;`;
-  let values = [searchQuery.search_query];
+  let sqlStatement = `SELECT * FROM ${table} WHERE location_id = $1;`;
+  let values = [data.id];
   return client.query(sqlStatement, values).then(data => {
-    console.log(`Search query returned: ${data.rowCount}`);
     //if data in database
     if (data.rowCount > 0) {
       return apiFetcher(
-        searchQuery.search_query,
-        searchQuery.latitude,
-        searchQuery.longitude
+        data.id,
+        data.latitude,
+        data.longitude
       );
     } else {
-      return dbFetcher(searchQuery);
+      return dbFetcher(data);
     }
   });
 }
 
-function weatherDbFetcher(searchQuery) {
+function weatherDbFetcher(data) {
   console.log('WeatherDbFetcher called');
   //go into weather table and return correct weather object
-  let sqlStatement = 'SELECT * FROM weather WHERE search_query = $1;';
-  let value = [searchQuery];
+  let sqlStatement = 'SELECT * FROM weather WHERE location_id = $1;';
+  let value = [data.id];
   return client.query(sqlStatement, value);
 }
 
-function weatherApiFetcher(searchQuery, latitude, longitude) {
+function weatherApiFetcher(data, latitude, longitude) {
   console.log('WeatchAPIFetcher called');
-  console.log(`latitude: ${latitude}, longitude ${longitude}`);
-  // try {
-  let dataFile = `https://api.darksky.net/forecast/${
-    process.env.DARKSKY_KEY
-  }/${latitude},${longitude}`;
-  superagent.get(dataFile).end((err, weatherApiResponse) => {
-    // console.log(weatherApiResponse);
-    let weatherForecastMap = weatherApiResponse.body.daily.data.map(element => {
-      return new Weather(element.summary, element.time);
+  try {
+    let dataFile = `https://api.darksky.net/forecast/${
+      process.env.DARKSKY_KEY
+    }/${latitude},${longitude}`;
+    superagent.get(dataFile).end((err, weatherApiResponse) => {
+      let weatherForecastMap = weatherApiResponse.body.daily.data.map(element => {
+        return new Weather(element.summary, element.time);
+      });
+      let insertStatement =
+        'INSERT INTO weather (location_id, forecast, weather_time) VALUES ( $1, $2, $3);';
+      let insertValue = [
+        data.id,
+        weatherForecastMap.forecast,
+        weatherForecastMap.time
+      ];
+      client.query(insertStatement, insertValue);
+      return weatherForecastMap;
     });
-    let insertStatement =
-      'INSERT INTO weather (search_query, forecast, weather_time) VALUES ( $1, $2, $3);';
-    let insertValue = [
-      searchQuery,
-      weatherForecastMap.forecast,
-      weatherForecastMap.time
-    ];
-    client.query(insertStatement, insertValue);
-    return weatherForecastMap;
-  });
-  // } catch(error){
-  //   console.log(error);
-  //   response.status(500).send('There is an error on our end sorry');
-  // }
+  } catch(error){
+    console.log(error);
+    response.status(500).send('There is an error on our end sorry');
+  }
+}
+
+function handleError(err, res) {
+  console.error(err);
+  if (res) res.status(500).send('Sorry, something went wrong');
 }
