a2bf18704214b15a05da6dc95e58e9817a4d4fb9
Paula Thomas <paula9t9@gmail.com>
2019-05-03 20:22:31 -0700

    weather finally bug free

--- server.js
+++ server.js
@@ -51,7 +51,6 @@ app.get('/location', (request, response) => {
       } else {
         let dataFile = `https://maps.googleapis.com/maps/api/geocode/json?address=${queryData}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
         superagent.get(dataFile).end((err, googleMapsApiResponse) => {
-          console.log(`googleMapsApiResponse: ${googleMapsApiResponse.body.status}`);
           const locationObject = new Location(
             queryData,
             googleMapsApiResponse.body
@@ -66,7 +65,6 @@ app.get('/location', (request, response) => {
           ];
           client.query(insertStatement, insertValue)
             .then(result => {
-              console.log(`SQL query result: ${result.rows[0].id}`);
               locationObject.id = result.rows[0].id;
               return locationObject;
             });
@@ -82,22 +80,14 @@ app.get('/location', (request, response) => {
 
 app.get('/weather', (request, response) => {
   try {
-    console.log(`lookup function returned: ${Object.values(lookupFunction(
+    lookupFunction(
       request.query.data,
       'weather',
       weatherDbFetcher,
       weatherApiFetcher
-    ))}`);
-    response
-      .status(200)
-      .send(
-        lookupFunction(
-          request.query.data,
-          'weather',
-          weatherDbFetcher,
-          weatherApiFetcher
-        )
-      );
+    ).then (weatherData => {
+      response.status(200).send(weatherData);
+    });
   }catch (error) {
     console.error(error);
     response.status(500).send('Sorry, something went wrong');
@@ -127,19 +117,13 @@ app.get('/events', (request, response) => {
 
 function lookupFunction(locationData, table, dbFetcher, apiFetcher) {
   //if locations contains searchQuery, execute other functions to fetch their data
-  console.log(`locationData: ${locationData.id}`);
   let sqlStatement = `SELECT * FROM ${table} WHERE location_id = $1;`;
   let values = [locationData.id];
   return client.query(sqlStatement, values).then(data => {
     //if data in database
-    console.log(`data.rowCount: ${data.rowCount}`);
     if (data.rowCount > 0) {
-      console.log(`dbFetcher returned: ${Object.values(dbFetcher(data.id))}`);
-      return dbFetcher(data.id);
+      return data.rows;
     } else {
-      console.log(`apiFetcher returned: ${Object.values(apiFetcher(locationData.id,
-        locationData.latitude,
-        locationData.longitude))}`);
       return apiFetcher(
         locationData.id,
         locationData.latitude,
@@ -150,26 +134,21 @@ function lookupFunction(locationData, table, dbFetcher, apiFetcher) {
 }
 
 function weatherDbFetcher(id) {
-  console.log('WeatherDbFetcher called');
   //go into weather table and return correct weather object
   let sqlStatement = 'SELECT * FROM weather WHERE location_id = $1;';
   let value = [id];
-  return client.query(sqlStatement, value);
+  return client.query(sqlStatement, value)
+    .then( dbResult => dbResult.rows);
 }
 
 function weatherApiFetcher(id, latitude, longitude) {
-  console.log('WeatchAPIFetcher called');
-  console.log(`location id: ${id}`);
   try {
-    console.log(`latitude: ${latitude}, longitude: ${longitude}`);
     let dataFile = `https://api.darksky.net/forecast/${process.env.DARKSKY_KEY}/${latitude},${longitude}`;
 
-    superagent.get(dataFile).end((err, weatherApiResponse) => {
-      console.log(`weatherApiResponse: ${weatherApiResponse}`);
+    return superagent.get(dataFile).end((err, weatherApiResponse) => {
       let weatherForecastMap = weatherApiResponse.body.daily.data.map(element => {
         return new Weather(element.summary, element.time);
       });
-      console.log(weatherForecastMap);
       weatherForecastMap.forEach(element => {
         let insertStatement =
         'INSERT INTO weather (location_id, forecast, weather_time) VALUES ( $1, $2, $3);';
